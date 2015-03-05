f756a71ed9ccb1855771d5c8ae3106f29eea9e3c
Arif Khokar <arif.khokar@rackspace.com>
2015-03-05 00:36:40 -0500

    yenc by section instead of by line
    Instead of inserting the escape characters after the line is encoded,
    split the original line at the positions where escape characters would
    fall and encode each section individually.  This addresses the
    performance issue of the previous version which was caused by creating
    and destroying numerous string literals in a loop.

--- tr_yenc.pl
+++ tr_yenc.pl
@@ -11,44 +11,74 @@ sub yenc {
     my $output = $_[1];
 
     my $line = '';
-    my $enc_line = '';
     my $bytes_processed = 0;
     my $crc = 0;
+    my $eq = "=";
     my $crlf = "\r\n";
     my @esc_pos = ();
-    while (my $cnt = read($input, $line, 128) || $enc_line ne '') {
+    my @enc_section = ();
+    my @remaining_section = ();
+    while (my $cnt = read($input, $line, 128) || length $remaining_section[0]) {
+        $line = join "", (@remaining_section, $line) if @remaining_section;
+        @remaining_section = ();
         $bytes_processed += $cnt;
         $crc = crc32($line, $crc);
-        $enc_line = "$enc_line${yenc_string(\$line)}";
-        my $insert_idx;
+        @esc_pos = get_esc_positions(\$line);
+        if (@esc_pos) {
+            my $enc_length = 0;
+            my @section = unpack get_unpack_str(\@esc_pos), $line;
+            while (my ($idx, $section) = each @section) {
+                $enc_length += length $section;
+                if ($idx == 0) {
+                    if ($enc_length > 127) {
+                        # Split at character 128
+                        my ($f, $s) = unpack "a128 a*", $section;
+                        push @enc_section, (yenc_string(\$f), \$crlf);
+                        push @remaining_section, $s;
+                        last;
+                    }
+                    push @enc_section, yenc_string(\$section);
+                } else {
+                    # Account for escape character
+                    ++$enc_length;
+                    if ($enc_length > 127) {
+                        my $diff = 127 - $enc_length + (length $section) + 1;
 
-        if (length($enc_line) < 128) {
-            $insert_idx = length($enc_line);
-            $enc_line = insert(\$crlf, $insert_idx, \$enc_line);
+                        # If the difference between 127 and the encoded length
+                        # so far plus the length of the next section with its
+                        # escape character is zero, that means that the line
+                        # ends with an escape character and the character after
+                        # that should be included when encoding this section.
+                        ++$diff if $diff == 0;
+                        my ($f, $s) = unpack "a$diff a*", $section;
+                        push @enc_section, (\$eq, yenc_string(\$f), \$crlf);
+                        push @remaining_section, $s;
+                        last;
+                    }
+                    push @enc_section, (\$eq, yenc_string(\$section));
+
+                    # If we're on the last section and the encoded lenght is
+                    # less than or equal to 127, then append a CRLF
+                    if ($enc_length <= 127 && $idx == $#section) {
+                        push @enc_section, \$crlf;
+                    }
+                }
+            }
         } else {
-            # Determine where to insert the CRLF. If (zero-based)
-            # position 127 is an '=' character, then add CRLF at
-            # position 129. Otherwise, add it at position 128.
-            my $end_chr = unpack "x127 a1", $enc_line;
-            $insert_idx = $end_chr eq '=' ? 129 : 128;
-            $enc_line = insert(\$crlf, $insert_idx, \$enc_line);
+            my ($f, $s) = unpack "a128 a*", $line;
+            push @enc_section, yenc_string(\$f), \$crlf;
+            push @remaining_section, $s;
         }
-
-        # Print the line
-        my $eol = $insert_idx + 2;
-        print $output unpack "a$eol", $enc_line;
-        last if length($enc_line) < 128;
-        $enc_line = unpack "x$eol a*", $enc_line;
+        print map $$_, @enc_section;
+        @enc_section = ();
     }
     return ($bytes_processed, $crc);
 }
 
-
-sub yenc_string {
+sub get_esc_positions {
 
     my $str = $_[0];
 
-    my $eq = '=';
     my @pos_list = ();
 
     # These ascii codes in the original string will result in characters
@@ -60,6 +90,31 @@ sub yenc_string {
     while ($$str =~ /[\xd6\xe0\xe3\x13]/g) {
         push @pos_list, (pos($$str) - 1);
     }
+    return @pos_list;
+}
+
+
+sub get_unpack_str {
+
+    my $pos_list = $_[0];
+
+    my @diff_list = ();
+    my ($first, $second) = (0, 1);
+    while ($second <= $#{$pos_list}) {
+        push @diff_list, "a" . ($$pos_list[$second] - $$pos_list[$first]);
+        ++$first;
+        ++$second;
+    }
+    my $unpack_str = "a$$pos_list[0] ";
+    $unpack_str .= join " ", @diff_list;
+    $unpack_str .= " a*";
+    return $unpack_str;
+}
+
+
+sub yenc_string {
+
+    my $str = $_[0];
 
     # This tr will yenc characters including those that need to be
     # escaped yenc is ascii (value + 42) % 256.
@@ -78,13 +133,9 @@ sub yenc_string {
     # ascii 228 through 255 -> ascii 14 through 41
     $$str =~ tr/\x00-\xff/*-<}>-\xff@\x01-\x09J\x0b\x0cM\x0e-)/;
 
-    # Now add escape characters
-    # for my $pos (sort { $b <=> $a } @$esc_positions) {
-    for my $pos (sort { $b <=> $a } @pos_list) {
-        $$str = insert(\$eq, $pos, $str);
-    }
     return $str;
 }
+
 
 sub yenc_header {
 
@@ -92,18 +143,11 @@ sub yenc_header {
     return "=ybegin line=$line size=$size name=$name\r\n";
 }
 
+
 sub yenc_footer {
 
     my ($size, $crc32) = @_;
     return "=yend size=$size crc32=" . sprintf "08%x", $crc32 . "\r\n";
-}
-
-
-sub insert {
-    my ($chr, $idx, $str) = @_;
-
-    my ($first, $second) = unpack "a$idx a*", $$str;
-    return join '', ($first, $$chr, $second);
 }
 
 
